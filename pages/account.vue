dae5549ab887d9916d49c7e535c8f740172d406e
Krish_09 <krishnateja.tumma@gmail.com>
2020-01-01 11:33:22 +0530

    Created account file....Not yet done
    Signed-off-by: Krish_09 <krishnateja.tumma@gmail.com>

---
+++ pages/account.vue
@@ -0,0 +1,32 @@
+<template>
+<section class="section">
+    <particles-bg num=50 type="cobweb" :bg="true" />
+    
+</section>
+</template>
+
+<style lang="scss" scoped>
+</style>
+
+<script>
+import {
+    ParticlesBg
+} from "particles-bg-vue";
+export default {
+    components: {
+        ParticlesBg
+    },
+
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+
+    methods: {
+
+    }
+
+}
+</script>
