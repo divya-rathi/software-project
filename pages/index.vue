eea270e0c161c5593841629337b9cc120235819a
Krish_09 <krishnateja.tumma@gmail.com>
2020-01-11 16:39:08 +0530

    Done with slide show in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,138 +1,112 @@
 <template>
-<section class="section edo-theme">
-    <div class="carousel">
-        <div class="card top">
-            <div class="card-image">
-                <figure class="image">
-                    <img src="https://bulma.io/images/placeholders/600x480.png" alt="Placeholder image">
-                </figure>
-            </div>
-            <div class="card-content">
-                <div class="media">
-                    <div class="media-content">
-                        <p class="title is-4">{{courseCode1}}</p>
-                        <p class="subtitle is-6">@johnsmith</p>
+<div class>
+    <hooper :settings="hooperSettings">
+        <slide class="has-text-centered" v-for="(code,index) in courseCodeList" :key="index">
+            <div class="card">
+                <div class="card-image">
+                    <figure class="image">
+                        <img src="https://bulma.io/images/placeholders/600x480.png" alt="Placeholder image" />
+                    </figure>
+                </div>
+                <div class="card-content">
+                    <div class="media">
+                        <div class="media-content">
+                            <p class="title is-4 has-text-centered is-family-primary">{{code}}</p>
+                            <p class="subtitle has-text-centered is-family-primary is-5">{{courseNameList[index]}}</p>
+                        </div>
                     </div>
-                </div>
-                <div class="content">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Phasellus nec iaculis mauris.
-                </div>
-                <router-link :to="{name:'course'}" class="btn btn-primary btn-md btn-rounded" @click.native="setcourseCode(courseCode1)">View</router-link>
-            </div>
-        </div>
-
-        <div class="card top">
-            <div class="card-image">
-                <figure class="image">
-                    <img src="https://bulma.io/images/placeholders/600x480.png" alt="Placeholder image">
-                </figure>
-            </div>
-            <div class="card-content">
-                <div class="media">
-                    <div class="media-content">
-                        <p class="title is-4">{{courseCode2}}</p>
-                        <p class="subtitle is-6">@johnsmith</p>
+                    <div class="content">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Phasellus nec iaculis mauris.
                     </div>
+                    <router-link :to="{name:'course'}" class="button is-family-sans-serif" @click.native="setcourseCode(code)">Goto Course</router-link>
                 </div>
-                <div class="content">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Phasellus nec iaculis mauris.
-                </div>
-               <router-link :to="{name:'course'}" class="btn btn-primary btn-md btn-rounded" @click.native="setcourseCode(courseCode2)">View</router-link>
             </div>
-        </div>
-
-        <div class="card top">
-            <div class="card-image">
-                <figure class="image">
-                    <img src="https://bulma.io/images/placeholders/600x480.png" alt="Placeholder image">
-                </figure>
-            </div>
-            <div class="card-content">
-                <div class="media">
-                    <div class="media-content">
-                        <p class="title is-4">{{courseCode3}}</p>
-                        <p class="subtitle is-6">@johnsmith</p>
-                    </div>
-                </div>
-                <div class="content">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Phasellus nec iaculis mauris.
-                </div>
-               <router-link :to="{name:'course'}" class="btn btn-primary btn-md btn-rounded" @click.native="setcourseCode(courseCode3)">View</router-link>
-            </div>
-        </div>
-
-    </div>
-
-</section>
+        </slide>
+        <hooper-navigation slot="hooper-addons"></hooper-navigation>
+    </hooper>
+</div>
 </template>
 
 <style lang="scss" scoped>
-.carousel {
-    display: flex;
+.hooper {
     height: auto;
-    margin-top: 60px;
-    margin-bottom: 100px;
 }
 
-.top {
-    margin-left: auto;
-    margin-right: auto;
-    height: auto;
-    width: 25%;
-}
-
-a.btn.btn-primary {
-    color: #fff;
-    font-size: 14px;
-}
-
-.btn.btn-md {
-    padding: .7rem 1.6rem;
-    font-size: .7rem
-}
-
-.btn-md {
-    padding-top: .58rem;
-    padding-bottom: .58rem
-}
-
-.btn-rounded {
-    -webkit-border-radius: 10em;
-    border-radius: 10em
-}
-
-.btn-primary {
-    background-color: #4285f4 !important;
-    color: #fff;
-}
-
-.btn {
-    display: inline-block;
-    padding: 6px 12px;
-    text-align: center;
-    text-transform: uppercase;
+.card {
+    margin: 2rem;
 }
 </style>
 
 <script>
+import {
+    Hooper,
+    Slide,
+    Navigation as HooperNavigation
+} from "hooper";
+import "hooper/dist/hooper.css";
+import {
+    fireDb
+} from "@/services/firebase";
+
 export default {
-    components: {},
-    data(){
-        return{
-            courseCode1:"15CSE111",
-            courseCode2:"15CSE102",
-            courseCode3:"15CSE100",
+    components: {
+        Hooper,
+        Slide,
+        HooperNavigation
+    },
+    data() {
+        return {
+            hooperSettings: {
+                mouseDrag: false,
+                touchDrag: false,
+                breakpoints: {
+                    300: {
+                        centerMode: false,
+                        itemsToShow: 1,
+                        itemsToSlide: 1
+                    },
+                    700: {
+                        centerMode: false,
+                        itemsToShow: 2,
+                        itemsToSlide: 2
+                    },
+                    1000: {
+                        transition: 500,
+                        centerMode: false,
+                        itemsToShow: 3,
+                        itemsToSlide: 3,
+                        mouseDrag: false
+                    }
+                }
+            },
+            courseCode1: "15CSE111",
+            courseCode2: "15CSE102",
+            courseCode3: "15CSE111",
+            courseCodeList: [],
+            courseNameList: []
+        };
+    },
+
+    methods: {
+        setcourseCode(courseCode) {
+            this.$store.dispatch("course/search", courseCode);
+            console.log("hello");
         }
     },
-    methods:{
-        setcourseCode(courseCode){
-            this.$store.dispatch('course/search',courseCode); 
-        },
+    mounted() {
+        // what to do to connect to db??
+        fireDb
+            .collection("courses")
+            .get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    this.courseCodeList.push(doc.id);
+                    this.courseNameList.push(doc.data().courseName);
+                });
+            });
+        console.log(this.courseCodeList);
+        console.log(this.courseNameList);
     }
-    
-    
-}
+};
 </script>
