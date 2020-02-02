00343873b8d26e75bf35642e32ec450ff1600630
Krish_09 <krishnateja.tumma@gmail.com>
2020-02-02 10:46:31 +0530

    Modified login page and other tweeks

--- pages/course.vue
+++ pages/course.vue
@@ -1,49 +1,49 @@
 <template>
 <div class="hero is-fullheight">
-    <div v-if="!visible">
-        <h1 class="has-text-centered is-size-1 is-family-sans-serif title">{{courseCode}} - {{ courseDetails.courseName }}</h1>
-        <div class="hero-body">
-            <div class="container">
-                <div class="columns">
-                    <div class="column card is-hidden-mobile">
-                        <div class="card-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae reiciendis praesentium alias excepturi mollitia libero quas dicta repudiandae a nemo numquam nobis neque nostrum laborum, laudantium sint vel molestiae sed!</div>
-                        <div class="has-text-centered">Rating comes here</div>
+    <h1 class="has-text-centered is-size-1 is-family-sans-serif title">{{courseCode}} - {{ courseDetails.courseName }}</h1>
+    <div class="hero-body">
+        <div class="container">
+            <div class="columns">
+                <div class="column card is-hidden-mobile">
+                    <div class="card-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae reiciendis praesentium alias excepturi mollitia libero quas dicta repudiandae a nemo numquam nobis neque nostrum laborum, laudantium sint vel molestiae sed!</div>
+                    <div class="has-text-centered">Rating comes here</div>
+                </div>
+                <div class="column is-three-quarters description">
+                    <div v-for="(key,val) in courseDetails.courseDescription.units" :key="val" class="content">
+                        <p class="title is-size-4 is-family-monospace has-text-centered">{{val}}</p>
+                        <p class="subtitle is-size-4 is-family-monospace has-text-centered">{{key}}</p>
+                        <hr />
                     </div>
-                    <div class="column is-three-quarters description">
-                        <div v-for="(key,val) in courseDetails.courseDescription.units" :key="val" class="content">
-                            <p class="title is-size-4 is-family-monospace has-text-centered">{{val}}</p>
-                            <p class="subtitle is-size-4 is-family-monospace has-text-centered">{{key}}</p>
+
+                    <div v-if="Object.keys(courseDetails.courseDescription.textbooks).length !== 0">
+                        <h2 class="is-size-3 has-text-centered is-family-sans-serif">Text Books</h2>
+                        <br />
+                        <div v-for="val in courseDetails.courseDescription.textbooks" :key="val" class="content">
+                            <p class="subtitle is-size-4 is-family-monospace has-text-centered">{{val}}</p>
                             <hr />
                         </div>
+                    </div>
+                    <div v-if="Object.keys(courseDetails.courseDescription.references).length !== 0">
+                        <h2 class="is-size-3 has-text-centered is-family-sans-serif">References</h2>
+                        <br />
+                        <div v-for="val in courseDetails.courseDescription.references" :key="val" class="content">
+                            <p class="subtitle is-size-4 is-family-monospace has-text-centered">{{val}}</p>
+                            <hr />
+                        </div>
+                    </div>
 
-                        <div v-if="Object.keys(courseDetails.courseDescription.textbooks).length !== 0">
-                            <h2 class="is-size-3 has-text-centered is-family-sans-serif">Text Books</h2>
-                            <br />
-                            <div v-for="val in courseDetails.courseDescription.textbooks" :key="val" class="content">
-                                <p class="subtitle is-size-4 is-family-monospace has-text-centered">{{val}}</p>
-                                <hr />
-                            </div>
-                        </div>
-                        <div v-if="Object.keys(courseDetails.courseDescription.references).length !== 0">
-                            <h2 class="is-size-3 has-text-centered is-family-sans-serif">References</h2>
-                            <br />
-                            <div v-for="val in courseDetails.courseDescription.references" :key="val" class="content">
-                                <p class="subtitle is-size-4 is-family-monospace has-text-centered">{{val}}</p>
-                                <hr />
-                            </div>
-                        </div>
-
-                        <div class="control has-text-centered">
-                            <button type="submit" class="button is-dark is-medium is-family-monospace">
-                                <nuxt-link to='/Q&A'>Q&A</nuxt-link>
-                            </button>
-                            <button type="submit" class="button is-dark is-medium is-family-monospace"><nuxt-link to='/review'>Reviews</nuxt-link></button>
-                        </div>
+                    <div class="control has-text-centered">
+                        <button type="submit" class="button is-dark is-medium is-family-monospace">
+                            <nuxt-link to='/Q&A'>Q&A</nuxt-link>
+                        </button>
+                        <button type="submit" class="button is-dark is-medium is-family-monospace">
+                            <nuxt-link to='/review'>Reviews</nuxt-link>
+                        </button>
                     </div>
                 </div>
             </div>
         </div>
-    </div>  
+    </div>
 </div>
 </template>
 
@@ -71,7 +71,6 @@
 .title {
     padding-top: calc(1.5rem + 2vh);
 }
-
 </style>
 
 <script>
@@ -91,9 +90,6 @@ export default {
     },
     data() {
         return {
-            visible: false,
-            visible1: false,
-            visible2: false,
             rating: 0,
             review: "",
             isStudent: true
