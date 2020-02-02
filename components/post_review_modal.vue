d3bb95c0ac1227d0caab57c894f1d225123ad625
Krish_09 <krishnateja.tumma@gmail.com>
2020-02-02 10:19:56 +0530

    fforgot to add * 😝

--- components/post_review_modal.vue
+++ components/post_review_modal.vue
@@ -5,7 +5,7 @@
             <div class="card">
                 <div class="is-size-3  is-family-secondary ">Rate the course..</div>
                 <div class="stars">
-                    <!-- <star-rating v-model="rating" show-rating="false" star-size=40 inactive-color="black" glow=2></star-rating> -->
+                    <star-rating v-model="rating" show-rating="false" star-size=40 inactive-color="black" glow=2></star-rating>
                 </div>
                 <br>
                 <form action method="post" @submit.prevent="postReview">
