44d8c17c6f115f86200c77aa51ee18ad8fc2281f
harini2642000 <32165816+harini2642000@users.noreply.github.com>
2020-03-14 23:50:51 +0530

    path

--- browser.py
+++ browser.py
@@ -2,6 +2,7 @@ import os
 import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 
 DRIVER = os.getenv('DRIVER', 'headless_chrome')
 BASE_URL = os.getenv('BASE_URL', 'http://backend:3000')
@@ -14,11 +15,13 @@ def get_chrome_driver():
 
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--user-data-dir=/tmp/browserdata/chrome --disable-plugins --disable-instant-extended-api")
-
+chrome_options = Options()  
+chrome_options.add_argument("--headless")  
+chrome_options.binary_location = '.\chromedriver.exe'  
     desired_capabilities.update(chrome_options.to_capabilities())
 
     browser = webdriver.Chrome(
-        executable_path='chromedriver',
+        executable_path=os.path.abspath("chromedriver.exe"),
         desired_capabilities=desired_capabilities)
 
     # Desktop size
