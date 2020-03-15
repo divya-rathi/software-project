d66b627be75d46c06bef729a55669983af67165c
harini2642000 <32165816+harini2642000@users.noreply.github.com>
2020-03-15 10:36:23 +0530

    uitest

--- browser.py
+++ browser.py
@@ -1,10 +1,15 @@
 import os
 import unittest
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options 
+from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver import Firefox
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support import expected_conditions as expected
+from selenium.webdriver.support.wait import WebDriverWait
+
 DRIVER = os.getenv('DRIVER', 'headless_chrome')
-BASE_URL = os.getenv('BASE_URL', 'http://backend:3000')
+BASE_URL = os.getenv('BASE_URL', 'https://pro-deploy-123.netlify.com/login')
 SELENIUM = os.getenv('SELENIUM', 'http://localhost:4444/wd/hub')
 
 
@@ -32,16 +37,9 @@ def get_chrome_driver():
 
 
 def get_headless_chrome():
-    desired_capabilities = webdriver.DesiredCapabilities.CHROME
-    desired_capabilities['loggingPrefs'] = {'browser': 'chrome'}
-
-    chrome_options = Options()
-    chrome_options.add_argument("--user-data-dir=/tmp/browserdata/chrome  --disable-plugins --disable-instant-extended-api   --headless")
-
-    desired_capabilities.update(chrome_options.to_capabilities())
-    chrome_driver = os.getcwd() +"\\chromedriver.exe"
-
-    browser = webdriver.Remote(executable_path=chrome_driver,desired_capabilities=desired_capabilities)
+    options = Options()
+    options.add_argument('-headless')
+    browser = Firefox(executable_path='geckodriver', options=options) 
 
     # Desktop size
     browser.set_window_position(0, 0)
