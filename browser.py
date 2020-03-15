e7799dd3d0428cd9fdbbce59b0a67ab26b38c19a
harini2642000 <32165816+harini2642000@users.noreply.github.com>
2020-03-15 19:46:54 +0530

    firfox install

--- browser.py
+++ browser.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support import expected_conditions as expected
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 import os
 DRIVER = os.getenv('DRIVER', 'headless_chrome')
 BASE_URL = os.getenv('BASE_URL', 'https://pro-deploy-123.netlify.com/login')
@@ -16,7 +17,9 @@ def get_headless_chrome():
     options.add_argument('-headless')
     options.headless=True
     driver_path = os.getcwd() + '/geckodriver'
-    browser = Firefox(executable_path=driver_path,firefox_options=options)
+    binary_path = os.getcwd() + '/firefox/firefox'
+    binary = FirefoxBinary(binary_path)
+    browser = Firefox(executable_path=driver_path,firefox_options=options,firefox_binary=binary)
     # Desktop size
     browser.set_window_position(0, 0)
     browser.set_window_size(1366, 768)
